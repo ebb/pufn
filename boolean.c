da5a5069248e89798c3891a36d5dcc267d07b2bf
Eric Bergstrome <eric.bergstrome@gmail.com>
2008-05-06 17:28:47 -0400

    Made boolean_new() static.

--- boolean.c
+++ boolean.c
@@ -6,16 +6,16 @@
 object_t boolean_t;
 object_t boolean_f;
 
-void boolean_initialize() {
-    boolean_t = boolean_new(1);
-    boolean_f = boolean_new(0);
-}
-
-object_t boolean_new(int value) {
+static object_t boolean_new(int value) {
     object_t self;
     self.tag = OBJECT_BOOLEAN_TAG;
     self.data.number = value;
     return self;
+}
+
+void boolean_initialize() {
+    boolean_t = boolean_new(1);
+    boolean_f = boolean_new(0);
 }
 
 int boolean_unbox(object_t self) {
--- boolean.h
+++ boolean.h
@@ -3,5 +3,4 @@
 object_t boolean_t;
 object_t boolean_f;
 void boolean_initialize();
-object_t boolean_new(int);
 int boolean_unbox(object_t);
